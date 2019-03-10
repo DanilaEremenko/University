d319406bd81b90cc4943086e6f7f68ef1d33277b
danila <danilaeremenko@mail.ru>
2019-03-10 20:53:11 +0300

    -some fixes, commentaries and report updates

--- Telecom/Lab_1/Lab_1.py
+++ Telecom/Lab_1/Lab_1.py
@@ -3,19 +3,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def get_sin_sig(t, freeq, ampl):
-    return ampl * np.cos(2 * np.pi * freeq * t) + ampl * np.cos(2 * 2 * np.pi * freeq * t)
+def get_sin_sig(t, freq, ampl):
+    """
+    :return: ampl * sin(freeq * t) + ampl * sin(2 * freeq * t)
+    """
+    return ampl * np.cos(2 * np.pi * freq * t) + ampl * np.cos(2 * 2 * np.pi * freq * t)
 
 
-def get_rect_sig(t, freeq, ampl):
-    return ampl * np.sign(get_sin_sig(t, freeq, ampl))
+def get_rect_sig(t, freq, ampl):
+    """
+    :return: ampl * np.sign(get_sin_sig(t, freeq, ampl))
+    """
+    return ampl * np.sign(get_sin_sig(t, freq, ampl))
 
 
-def plot_graphic(x, y, title, x_label="x", y_label="y", show=False, save=False):
+def plot_graphic(x, y, title, x_label="x", y_label="y", xlim=None, ylim=None, show=False, save=False):
+    """
+    just my function for comfortable plotting
+    """
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.title(title)
     plt.plot(x, y)
+
+    if xlim != None:
+        plt.xlim(xlim[0], xlim[1])
+    if ylim != None:
+        plt.ylim(ylim[0], ylim[1])
+
     if show:
         plt.show()
     if save:
@@ -25,25 +40,29 @@ def plot_graphic(x, y, title, x_label="x", y_label="y", show=False, save=False):
 
 if __name__ == '__main__':
 
-    freeq = 20
-    ampl = 2
+    sig_freq = 20  # frequency of original signal
+    ampl = 2  # amplitude of original signal
     fs = 1000  # sampling rate
     ts = 1.0 / fs  # sampling interval
     n = 1 << 13  # number of fft points, pick power of 2
-    t = np.arange(0, n * ts, ts)  # time vector
-    pts_num = 100
-    signals = [get_sin_sig(t, freeq, ampl), get_rect_sig(t, freeq, ampl)]
-    functions = ['sinus_', 'rectangle_']
+
+    t = np.arange(0, n * ts, step=ts)  # time vector
+    signals = [get_sin_sig(t, sig_freq, ampl), get_rect_sig(t, sig_freq, ampl)]
+
+    functions_names = ['sinus_', 'rectangle_']
     show = True
     save = not show
 
-    for sig, title in zip(signals, functions):
-        sig_fft = np.fft.fft(sig) / n * 2  # discrete Fourier Transform
+    for sig, title in zip(signals, functions_names):
         fft_freq = np.fft.fftfreq(n, ts)  # discrete Fourier Transform frequencies
+        sig_fft = np.fft.fft(sig) / n * 2  # discrete Fourier Transform ( / n * 2 - normalization)
 
-        plot_graphic(x=t[:fs], y=sig[:fs], title=title + 'signal', x_label='time(S)',
-                     y_label='signal',
+        # [:(n - 1) / 2], because second half it's mirror image of first half
+        plot_graphic(x=t[:(n - 1) / 2], y=sig[:(n - 1) / 2], title=title + 'signal',
+                     x_label='time(S)', y_label='signal',
+                     xlim=[0, 1],
                      show=show, save=save)
-        plot_graphic(x=fft_freq[:fs], y=abs(sig_fft)[:fs], title=title + 'spectrum', x_label='frequency (Hz)',
-                     y_label='amplitude (V)',
+        plot_graphic(x=fft_freq[:(n - 1) / 2], y=abs(sig_fft)[:(n - 1) / 2], title=title + 'spectrum',
+                     x_label='frequency (Hz)', y_label='amplitude (V)',
+                     xlim=[0, 150],
                      show=show, save=save)
