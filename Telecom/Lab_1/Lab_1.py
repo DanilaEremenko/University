8a8c33bbf6a47185414c7de5c7cb63e55b27852e
danila <danilaeremenko@mail.ru>
2019-03-11 19:23:42 +0300

    -report and code updated

--- Telecom/Lab_1/Lab_1.py
+++ Telecom/Lab_1/Lab_1.py
@@ -40,6 +40,7 @@ def plot_graphic(x, y, title, x_label="x", y_label="y", xlim=None, ylim=None, sh
 
 if __name__ == '__main__':
 
+    #Parameters of signals
     sig_freq = 20  # frequency of original signal
     ampl = 2  # amplitude of original signal
     fs = 1000  # sampling rate
@@ -49,20 +50,32 @@ if __name__ == '__main__':
     t = np.arange(0, n * ts, step=ts)  # time vector
     signals = [get_sin_sig(t, sig_freq, ampl), get_rect_sig(t, sig_freq, ampl)]
 
+
+    #Parameters for graphics
     functions_names = ['sinus_', 'rectangle_']
     show = True
     save = not show
 
+    #Spectrum calculating and plotting
     for sig, title in zip(signals, functions_names):
+
         fft_freq = np.fft.fftfreq(n, ts)  # discrete Fourier Transform frequencies
+
         sig_fft = np.fft.fft(sig) / n * 2  # discrete Fourier Transform ( / n * 2 - normalization)
 
         # [:(n - 1) / 2], because second half it's mirror image of first half
-        plot_graphic(x=t[:(n - 1) / 2], y=sig[:(n - 1) / 2], title=title + 'signal',
+        plot_graphic(
+                     x=t[:(n - 1) / 2], y=sig[:(n - 1) / 2], 
+                     title=title + 'signal',
                      x_label='time(S)', y_label='signal',
                      xlim=[0, 1],
-                     show=show, save=save)
-        plot_graphic(x=fft_freq[:(n - 1) / 2], y=abs(sig_fft)[:(n - 1) / 2], title=title + 'spectrum',
+                     show=show, save=save
+                    )
+
+        plot_graphic(
+                     x=fft_freq[:(n - 1) / 2], y=abs(sig_fft)[:(n - 1) / 2], 
+                     title=title + 'spectrum',
                      x_label='frequency (Hz)', y_label='amplitude (V)',
                      xlim=[0, 150],
-                     show=show, save=save)
+                     show=show, save=save
+                    )
