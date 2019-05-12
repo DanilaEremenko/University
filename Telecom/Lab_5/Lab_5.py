9b5192bb7822a55c9cdd265dec4f45bab8bb7ac5
danila <danilaeremenko@mail.ru>
2019-05-12 11:43:39 +0300

    -Lab_5 legends fixed + Lab_5_report done

--- Telecom/Lab_5/Lab_5.py
+++ Telecom/Lab_5/Lab_5.py
@@ -12,39 +12,37 @@ if __name__ == '__main__':
     n = 1 << 13  # number of fft points, pick power of 2
 
     t = np.arange(0, n * ts, step=ts)  # time vector
-    sig = np.sin(2 * np.pi * sig_freq * t)
+    sig = sig_ampl * np.sin(2 * np.pi * sig_freq * t)
 
     # ----------------------------- Parameters of modulation signal ------------------------------
     carrier_freq = 50
-    carrier_amplitude = 1
-    phase_modulated = carrier_amplitude * np.sin(2 * np.pi * carrier_freq * t + sig_ampl * sig)
-
+    carrier_amplitude = sig_ampl
     # ----------------------------- Parameters of plotting ------------------------------
     sig_xlim = (0, 0.25)
 
+    # ----------------------------- main part ------------------------------------------
+    phase_modulated = carrier_amplitude * np.sin(2 * np.pi * carrier_freq * t + sig)
 
     sig_integrated = np.zeros_like(sig)
     for i, dt in enumerate(t):
         sig_integrated[i] = integrate.simps(sig, dx=t[i])
 
-    freq_modulated = carrier_amplitude * np.sin(2 * np.pi * carrier_freq * t
-                                                + sig_ampl * sig_integrated)
+    freq_modulated = carrier_amplitude * np.sin(2 * np.pi * carrier_freq * t + sig_ampl * sig_integrated)
 
     analytic_signal = hilbert(phase_modulated)
     phase_function = np.unwrap(np.angle(analytic_signal) + np.pi / 2)
 
     phase_demodulated = phase_function - 2 * np.pi * carrier_freq * t
-
     freq_demodulated = phase_function - 2 * np.pi * carrier_freq * t
 
     fft_freq = np.fft.fftfreq(n, ts)  # python function to get Hz frequency axis
 
-    phase_modulated_fft = \
-        abs(np.fft.fft(phase_modulated)) / n * 2  # discrete Fourier Transform ( / n * 2 - normalization)
+    # discrete Fourier Transform ( / n * 2 - normalization)
+    phase_modulated_fft = abs(np.fft.fft(phase_modulated)) / n * 2
     phase_demodulated_fft = abs(np.fft.fft(phase_demodulated)) / n * 2
 
-    freq_modulated_fft = \
-        abs(np.fft.fft(freq_modulated)) / n * 2  # discrete Fourier Transform ( / n * 2 - normalization)
+    # discrete Fourier Transform ( / n * 2 - normalization)
+    freq_modulated_fft = abs(np.fft.fft(freq_modulated)) / n * 2
     freq_demodulated_fft = abs(np.fft.fft(freq_demodulated)) / n * 2
 
     # ---------------------------- PLOTS -------------------------------------
@@ -57,19 +55,20 @@ if __name__ == '__main__':
                  xlim=sig_xlim,
                  x_label='time (s)', y_label='amplitude (V)', show=False)
 
-    plt.legend(('PM modulated sine wave', 'PM demodulated sine wave'), loc='upper right')
+    plt.legend(('phase modulated sine wave', 'phase demodulated sine wave'), loc='upper right')
     plt.show()
 
     # ------------------------------------------------------------------------
     plot_graphic(fft_freq, phase_modulated_fft,
                  xlim=(0, 250),
-                 x_label='time (s)', y_label='amplitude (V)', show=False)
+                 x_label='frequency (Hz)', y_label='amplitude (V)', show=False)
 
     plot_graphic(fft_freq, phase_demodulated_fft,
                  xlim=(0, 250),
-                 x_label='time (s)', y_label='amplitude (V)', show=False)
+                 x_label='frequency (Hz)', y_label='amplitude (V)', show=False)
 
-    plt.legend(('PM modulated sine wave', 'PM demodulated sine wave'), loc='upper right')  # TODO fix legends
+    plt.legend(('spectrum of phase modulated sine wave', 'spectrum of phase demodulated sine wave'),
+               loc='upper right')
     plt.show()
 
     # ------------------------------------------------------------------------
@@ -81,17 +80,19 @@ if __name__ == '__main__':
                  xlim=sig_xlim,
                  x_label='time (s)', y_label='amplitude (V)', show=False)
 
-    plt.legend(('PM modulated sine wave', 'PM demodulated sine wave'), loc='upper right')  # TODO fix legends
+    plt.legend(('frequency modulated sine wave', 'frequency demodulated sine wave'),
+               loc='upper right')
     plt.show()
 
     # ------------------------------------------------------------------------
     plot_graphic(t[1:], freq_modulated_fft[1:],
                  xlim=(0, 0.5),
-                 x_label='time (s)', y_label='amplitude (V)', show=False)
+                 x_label='frequency (Hz)', y_label='amplitude (V)', show=False)
 
     plot_graphic(t[1:], freq_demodulated_fft[1:],
                  xlim=(0, 0.5),
-                 x_label='time (s)', y_label='amplitude (V)', show=False)
+                 x_label='frequency (Hz)', y_label='amplitude (V)', show=False)
 
-    plt.legend(('PM modulated sine wave', 'PM demodulated sine wave'), loc='upper right')  # TODO fix legends
+    plt.legend(('spectrum of frequency modulated sine wave', 'spectrum of frequency demodulated sine wave'),
+               loc='upper right')
     plt.show()
